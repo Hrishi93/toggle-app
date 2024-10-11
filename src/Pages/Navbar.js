df40478c1bfbc9fd2c3abb521a7545d06a01dc0c
Hrishi93 <sonumahure@gmail.com>
2024-10-11 07:34:20 +0530

    Initial commit

---
+++ src/Pages/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+      <ul>
+        <li><a href="/Home">Home</a></li>
+        <li><a href="/About">About</a></li>
+        <li><a href="#">TV</a></li>
+        <li><a href="#">Mobile</a></li>
+        <li><a href="#">Laptop</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </div>
+  )
+}
